83227d5fea9e6b7422ff4a310602f5a906818eaf
Zheng Luo <helloworld09@163.com>
2017-04-17 22:27:11 +0800

    Test rdd

--- q1/pj1.py
+++ q1/pj1.py
@@ -1,61 +1,45 @@
-import random
-
-import numpy as np
 from pyspark import SparkContext, SparkConf
-from pyspark.mllib.linalg import Vectors
-from pyspark.sql import SQLContext
 
 conf = SparkConf().setAppName('q1')
 sc = SparkContext(conf=conf)
 
 data_path = ''
 
-# fn = open('training.csv', 'r')
-# trainFile = fn.readlines()
 # attrName = trainFile[0].strip().split(',')
-# trainData = []
-# for line in trainFile[1:100]:
-#     labels = line.strip().split(',')[:-1]
-#     labelTuple = []
-#     image = map(int, line.strip().split(',')[-1].split(' '))
-#     for label in labels:
-#         try:
-#             labelTuple.append(float(label))
-#         except:
-#             labelTuple.append(None)
-#     trainData.append(labelTuple.append(image))
+
+def toFloat(num):
+    try:
+        return float(num)
+    except:
+        return None
+
+raw_data = sc.textFile(data_path + 'training.csv').filter(lambda line: line[0] != 'l')
+train_data = raw_data.map(lambda line: line.strip().split(','))
+train_X = train_data.map(lambda l: map(int, l[-1].split(' ')))
+train_Y = train_data.map(lambda l: l[:-1]).map(lambda l: map(toFloat, l))
+
+print 'train count\t' + str(train_X.count())
+
+
+
+
+# from keras.models import Sequential
+# from keras.layers.core import Dense, Dropout, Activation
+# from keras.optimizers import SGD
 #
-# trainRDD = sc.parallelize(trainData)
-# print trainRDD.collect()
-
-sqlContext = SQLContext(sc)
-
-
-def shuffle_csv(csv_file):
-    lines = open(csv_file).readlines()
-    random.shuffle(lines)
-    open(csv_file, 'w').writelines(lines)
-
-
-def load_data_frame(csv_file, shuffle=True):
-    if shuffle:
-        shuffle_csv(csv_file)
-    data = sc.textFile(data_path + csv_file)  # This is an RDD, which will later be transformed to a data frame
-    data = data.filter(lambda x: x[0].isdigit()).map(lambda line: line.split(','))
-
-    def convertData(line):
-        image = map(int, line[-1].split(' '))
-        features = []
-        for feature in line[:-1]:
-            try:
-                features.append(float(feature))
-            except:
-                features.append(None)
-        return Vectors.dense(np.asarray(features).astype(np.float64)), image
-
-    data = data.map(convertData)
-    return sqlContext.createDataFrame(data, ['features', 'image'])
-
-train_df = load_data_frame("training.csv")
-print("Train data frame:")
-train_df.show(5)
+# model = Sequential()
+# model.add(Dense(128, input_dim=96*96))
+# model.add(Activation('relu'))
+# model.add(Dropout(0.2))
+# model.add(Dense(128))
+# model.add(Activation('relu'))
+# model.add(Dropout(0.2))
+# model.add(Dense(30))
+# model.compile(loss='categorical_crossentropy', optimizer=SGD())
+#
+# from elephas.spark_model import SparkModel
+# from elephas import optimizers as elephas_optimizers
+#
+# adagrad = elephas_optimizers.Adagrad()
+# spark_model = SparkModel(sc, model, optimizer=adagrad, frequency='epoch', mode='asynchronous', num_workers=1)
+# spark_model.train(trainRDD, nb_epoch=20, batch_size=trainRDD.count(), verbose=0, validation_split=0.1)
