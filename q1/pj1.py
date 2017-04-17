fbd420ea67c7d7dd15daf148927119d7d89bddeb
Zheng Luo <helloworld09@163.com>
2017-04-17 22:23:44 +0800

    Test

--- q1/pj1.py
+++ q1/pj1.py
@@ -1,6 +1,3 @@
-import random
-
-import numpy as np
 from pyspark import SparkContext, SparkConf
 
 conf = SparkConf().setAppName('q1')
@@ -8,36 +5,38 @@ sc = SparkContext(conf=conf)
 
 data_path = ''
 
-fn = open('training.csv', 'r')
-trainFile = fn.readlines()
-attrName = trainFile[0].strip().split(',')
-trainData = []
-for line in trainFile[1:100]:
-    labels = line.strip().split(',')[:-1]
-    labelTuple = []
-    image = map(int, line.strip().split(',')[-1].split(' '))
-    for label in labels:
-        try:
-            labelTuple.append(float(label))
-        except:
-            labelTuple.append(None)
-    trainData.append([labelTuple, image])
+# attrName = trainFile[0].strip().split(',')
 
-trainRDD = sc.parallelize(trainData)
-print(trainRDD.count())
+def toFloat(num):
+    try:
+        return float(num)
+    except:
+        return None
 
-from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation
-from keras.optimizers import SGD
+raw_data = sc.textFile(data_path + 'training.csv').filter(lambda line: line[0] != 'l')
+train_data = raw_data.map(lambda line: line.strip().split(','))
+train_X = train_data.map(lambda l: map(int, l[-1].split(' ')))
+train_Y = train_data.map(lambda l: l[:-1]).map(lambda l: map(toFloat, l))
 
-from elephas.utils.rdd_utils import to_simple_rdd
+print 'train count\t' + str(train_X.count())
 
-model = Sequential()
-model.add(Dense(128, input_dim=96*96))
-model.add(Activation('relu'))
-model.add(Dropout(0.2))
-model.add(Dense(128))
-model.add(Activation('relu'))
-model.add(Dropout(0.2))
-model.add(Dense(30))
-model.compile(loss='categorical_crossentropy', optimizer=SGD())
+# from keras.models import Sequential
+# from keras.layers.core import Dense, Dropout, Activation
+# from keras.optimizers import SGD
+#
+# model = Sequential()
+# model.add(Dense(128, input_dim=96*96))
+# model.add(Activation('relu'))
+# model.add(Dropout(0.2))
+# model.add(Dense(128))
+# model.add(Activation('relu'))
+# model.add(Dropout(0.2))
+# model.add(Dense(30))
+# model.compile(loss='categorical_crossentropy', optimizer=SGD())
+#
+# from elephas.spark_model import SparkModel
+# from elephas import optimizers as elephas_optimizers
+#
+# adagrad = elephas_optimizers.Adagrad()
+# spark_model = SparkModel(sc, model, optimizer=adagrad, frequency='epoch', mode='asynchronous', num_workers=1)
+# spark_model.train(trainRDD, nb_epoch=20, batch_size=trainRDD.count(), verbose=0, validation_split=0.1)
