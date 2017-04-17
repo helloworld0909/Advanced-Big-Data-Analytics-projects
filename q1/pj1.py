6c0e6bb5268d1b8add12b81a87dd341d530c54a9
Zheng Luo <helloworld09@163.com>
2017-04-17 19:50:19 +0800

    Decide to use elephas

--- q1/pj1.py
+++ q1/pj1.py
@@ -2,60 +2,42 @@ import random
 
 import numpy as np
 from pyspark import SparkContext, SparkConf
-from pyspark.mllib.linalg import Vectors
-from pyspark.sql import SQLContext
 
 conf = SparkConf().setAppName('q1')
 sc = SparkContext(conf=conf)
 
 data_path = ''
 
-# fn = open('training.csv', 'r')
-# trainFile = fn.readlines()
-# attrName = trainFile[0].strip().split(',')
-# trainData = []
-# for line in trainFile[1:100]:
-#     labels = line.strip().split(',')[:-1]
-#     labelTuple = []
-#     image = map(int, line.strip().split(',')[-1].split(' '))
-#     for label in labels:
-#         try:
-#             labelTuple.append(float(label))
-#         except:
-#             labelTuple.append(None)
-#     trainData.append(labelTuple.append(image))
-#
-# trainRDD = sc.parallelize(trainData)
-# print trainRDD.collect()
+fn = open('training.csv', 'r')
+trainFile = fn.readlines()
+attrName = trainFile[0].strip().split(',')
+trainData = []
+for line in trainFile[1:100]:
+    labels = line.strip().split(',')[:-1]
+    labelTuple = []
+    image = map(int, line.strip().split(',')[-1].split(' '))
+    for label in labels:
+        try:
+            labelTuple.append(float(label))
+        except:
+            labelTuple.append(None)
+    trainData.append([labelTuple, image])
 
-sqlContext = SQLContext(sc)
+trainRDD = sc.parallelize(trainData)
+print(trainRDD.count())
 
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.optimizers import SGD
 
-def shuffle_csv(csv_file):
-    lines = open(csv_file).readlines()
-    random.shuffle(lines)
-    open(csv_file, 'w').writelines(lines)
+from elephas.utils.rdd_utils import to_simple_rdd
 
-
-def load_data_frame(csv_file, shuffle=True):
-    if shuffle:
-        shuffle_csv(csv_file)
-    data = sc.textFile(data_path + csv_file)  # This is an RDD, which will later be transformed to a data frame
-    data = data.filter(lambda x: x[0].isdigit()).map(lambda line: line.split(','))
-
-    def convertData(line):
-        image = map(int, line[-1].split(' '))
-        features = []
-        for feature in line[:-1]:
-            try:
-                features.append(float(feature))
-            except:
-                features.append(None)
-        return Vectors.dense(np.asarray(features).astype(np.float64)), image
-
-    data = data.map(convertData)
-    return sqlContext.createDataFrame(data, ['features', 'image'])
-
-train_df = load_data_frame("training.csv")
-print("Train data frame:")
-train_df.show(5)
+model = Sequential()
+model.add(Dense(128, input_dim=96*96))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(Dense(30))
+model.compile(loss='categorical_crossentropy', optimizer=SGD())
